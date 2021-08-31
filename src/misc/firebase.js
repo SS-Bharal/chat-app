ad53eee80f6f5bf520d6a85aa6f7099ae59505f7
Sahil <sahilbharal0001@gmail.com>
2021-08-31 18:40:26 +0530

    initial

---
+++ src/misc/firebase.js
@@ -0,0 +1,51 @@
+import { Notification as Toast } from 'rsuite';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+import 'firebase/messaging';
+import 'firebase/functions';
+import { isLocalhost } from './helpers';
+
+const config = {
+  apiKey: "AIzaSyDjkB49HcjUjGnWjuC890V8LuiKeaksJeA",
+  authDomain: "chat-web-app-a41b0.firebaseapp.com",
+  databaseURL: "https://chat-web-app-a41b0-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-a41b0",
+  storageBucket: "chat-web-app-a41b0.appspot.com",
+  messagingSenderId: "262093263503",
+  appId: "1:262093263503:web:9e48956b595c41c23bd6fa",
+};
+// const config = {
+//   apiKey: 'AIzaSyAKGRssxY4OW9um6RlSbbQ1Fh_lgdWfoPs',
+//   authDomain: 'chat-web-app-4ee4c.firebaseapp.com',
+//   databaseURL: 'https://chat-web-app-4ee4c.firebaseio.com',
+//   projectId: 'chat-web-app-4ee4c',
+//   storageBucket: 'chat-web-app-4ee4c.appspot.com',
+//   messagingSenderId: '167319830934',
+//   appId: '1:167319830934:web:419e220f9fdcd15ea25db7',
+// };
+
+export const fcmVapidKey =
+  'BLs_I-HQyrAuUJJh8H3U0vtHGhVhXLMqoVoomeNL90GMKm0-o7sSoN9CJYRiBAVz-Yi7ZAni8mKateJfDwodTnw';
+
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
+export const storage = app.storage();
+export const functions = app.functions('europe-west3');
+
+export const messaging = firebase.messaging.isSupported()
+  ? app.messaging()
+  : null;
+
+if (messaging) {
+  messaging.onMessage(({ notification }) => {
+    const { title, body } = notification;
+    Toast.info({ title, description: body, duration: 0 });
+  });
+}
+
+if (isLocalhost) {
+  functions.useEmulator('localhost', 5001);
+}
