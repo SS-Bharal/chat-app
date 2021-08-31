a16d0215f9ab23e7b978f4aabafb18772d966d2a
Sahil <sahilbharal0001@gmail.com>
2021-08-31 21:16:58 +0530

    initial

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -7,16 +7,39 @@ import 'firebase/messaging';
 import 'firebase/functions';
 import { isLocalhost } from './helpers';
 
+// const config = {
+//   apiKey: process.env.APIKEY ,
+//   authDomain: process.env.AUTHDOMAIN ,
+//   databaseURL: process.env.DATABASEURL ,
+//   projectId: process.env.PROJECTID ,
+//   storageBucket: process.env.STORAGEBUCKET ,
+//   messagingSenderId: process.env.MESSAGINGSENDERID ,
+//   appId: process.env.APPID ,
+// };
+
+
+// OWN
+
 const config = {
-  apiKey: process.env.APIKEY ,
-  authDomain: process.env.AUTHDOMAIN ,
-  databaseURL: process.env.DATABASEURL ,
-  projectId: process.env.PROJECTID ,
-  storageBucket: process.env.STORAGEBUCKET ,
-  messagingSenderId: process.env.MESSAGINGSENDERID ,
-  appId: process.env.APPID ,
+  apiKey: 'AIzaSyDjkB49HcjUjGnWjuC890V8LuiKeaksJeA',
+  authDomain: 'chat-web-app-a41b0.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-a41b0-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-a41b0',
+  storageBucket: 'chat-web-app-a41b0.appspot.com',
+  messagingSenderId: '262093263503',
+  appId: '262093263503:web:9e48956b595c41c23bd6fa',
 };
 
+// Instructor 
+// const config = {
+//   apiKey: 'AIzaSyAKGRssxY4OW9um6RlSbbQ1Fh_lgdWfoPs',
+//   authDomain: 'chat-web-app-4ee4c.firebaseapp.com',
+//   databaseURL: 'https://chat-web-app-4ee4c.firebaseio.com',
+//   projectId: 'chat-web-app-4ee4c',
+//   storageBucket: 'chat-web-app-4ee4c.appspot.com',
+//   messagingSenderId: '167319830934',
+//   appId: '1:167319830934:web:419e220f9fdcd15ea25db7',
+// };
 
 export const fcmVapidKey =
   'BLs_I-HQyrAuUJJh8H3U0vtHGhVhXLMqoVoomeNL90GMKm0-o7sSoN9CJYRiBAVz-Yi7ZAni8mKateJfDwodTnw';
