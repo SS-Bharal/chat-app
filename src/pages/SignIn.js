88ab24b8c4e0aeeae338b4f9183c895c61235a81
Sahil <sahilbharal0001@gmail.com>
2021-09-01 09:26:23 +0530

    initial

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -36,24 +36,45 @@ const SignIn = () => {
           <Col xs={24} md={12} mdOffset={6}>
             <Panel>
               <div className="text-center">
-                <h2>Welcome to Chat</h2>
-                <p>Progressive chat platform for neophytes</p>
+                <h2>Welcome to Chat Web App</h2>
+                <p>Progressive chat platform for Students</p>
               </div>
 
               <div className="mt-3">
-                <Button block color="blue" onClick={onFacebookSignIn}>
-                  <Icon icon="facebook" /> Continue with Facebook
-                </Button>
+
 
                 <Button block color="green" onClick={onGoogleSignIn}>
                   <Icon icon="google" /> Continue with Google
                 </Button>
+                {/* <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" /> Continue with Facebook
+                </Button> */}
+                <br></br>
+                <br></br>
+                <br></br>
+                <center>
+                  <h4>Created With ❤️ By SAHIL BHARAL</h4>
+                  <br></br>
+                  <p>for any query contact :  </p>
+                  <p> SAHIL BHARAL  </p>
+                  <p> +91 7056099684 </p>
+                  <br></br>
+                  <br></br>
+                  <h6>Read:</h6>
+                  <p>chat in this app are public.</p>
+                  <p>Anyone who use this app can read all messages.</p>
+
+
+                </center>
+
               </div>
             </Panel>
           </Col>
         </Row>
       </Grid>
     </Container>
+
+
   );
 };
 
